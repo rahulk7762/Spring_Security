ad3721e228c7b21332b8785574cd1fec2953e455
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-05-08 16:05:20 +0530

    update

--- src/main/java/com/auth/config/SecurityConfig.java
+++ src/main/java/com/auth/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.auth.config;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,19 +11,52 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//		Disables CSRF protection.
-		http.csrf(customizer -> customizer.disable());
-//		Every request needs login.
-		http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//		Enables default login form of Spring Security
-		http.formLogin(Customizer.withDefaults());
-//		Enables Basic Authentication.
-		http.httpBasic(Customizer.withDefaults());
+////		Disables CSRF protection.
+//		http.csrf(customizer -> customizer.disable());
+////		Every request needs login.
+//		http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+////		Enables default login form of Spring Security
+////		http.formLogin(Customizer.withDefaults());
+////		Enables Basic Authentication.
+//		http.httpBasic(Customizer.withDefaults());
+////		Stateless Authentication
+//		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//		return http.build();
+
 		
-		return http.build();
+		
+		return http.csrf(Customizer -> Customizer.disable())
+				.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()).build();
+
 	}
 	
+	
+	
+	
+	// this is only when when we are not dealing witht the Database
+	
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//
+//	    UserDetails user1 = User
+//	            .withDefaultPasswordEncoder()
+//	            .username("rahul")
+//	            .password("Rahul@098")
+//	            .roles("USER")
+//	            .build();
+//	    
+//	    UserDetails user2 = User
+//	            .withDefaultPasswordEncoder()
+//	            .username("rohit")
+//	            .password("Rohit@098")
+//	            .roles("USER")
+//	            .build();
+//
+//
+//	    return new InMemoryUserDetailsManager(user1,user2);
+//	}
+
 }
